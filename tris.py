90150c20ba50adc46d0c485cae006f45eda3a776
ewen <ewen.brun@icloud.com>
2018-03-06 21:05:45 +0100

    Tri bulle en plus

--- tris.py
+++ tris.py
@@ -37,5 +37,9 @@ def triParInsertion(lst):
 
 
 def triBulle(lst):
-    """Tris par insertion."""
-    pass
+    """Tris bulle."""
+    for i in range(len(lst), 0, -1):
+        for j in range(1, i):
+            if lst[j] < lst[j-1]:
+                lst[j], lst[j-1] = lst[j-1], lst[j]
+    return lst
